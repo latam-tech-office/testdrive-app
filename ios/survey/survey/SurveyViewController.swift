efabb6d52339c2b88c2784c9edd80a906917c8c9
Mauricio "Maltron" Leal <maltron@gmail.com>
2018-03-09 09:35:40 -0300

    Early draft of Survey

--- ios/survey/survey/SurveyViewController.swift
+++ ios/survey/survey/SurveyViewController.swift
@@ -9,86 +9,105 @@
 import UIKit
 import CoreData
 
-struct MenuOption {
+class SurveyJSON: Codable {
+    let id: String
     let name: String
-    let description: String
+    let questions: [QuestionJSON]
+    
+    init(id: String, name: String, questions: [QuestionJSON]) {
+        self.id = id
+        self.name = name
+        self.questions = questions 
+    }
+}
+
+class QuestionJSON: Codable {
+    let order: Int
+    let question: String
+    let type: QuestionType
+    let answers: [AnswerJSON]
+    
+    init(order: Int, question: String, type: QuestionType, answers: [AnswerJSON]) {
+        self.order = order
+        self.question = question
+        self.type = type
+        self.answers = answers
+    }
+}
+
+enum QuestionType: String, Codable {
+    case single
+    case multiple
+    case rank
+    case open
+}
+
+class AnswerJSON: Codable {
+    let order: Int
+    let answer: String
+    
+    init(order: Int, answer: String) {
+        self.order = order
+        self.answer = answer
+    }
 }
 
 class SurveyViewController: UITableViewController {
     
-    var mytoolbar: UIToolbar!
-    var mytableView: UITableView!
+    // CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA
+    //   CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA CORE DATA
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container: NSPersistentContainer = NSPersistentContainer(name: "survey")
+        container.loadPersistentStores(completionHandler: { (description, error) in
+            if let error = error as NSError? {
+                fatalError("### persistentContainer UNABLE TO LOAD DATA: \(error)")
+            }
+        })
     
-    let o: [[MenuOption]] = [
-        [ MenuOption(name: "hello", description: "desc") ]
-    ]
-    
-    let options: [[String]] = [
-        // Section 0
-        [ "Admin Users"],
-        // Secion 1
-        [ "Create", "Simulate"]
-    ]
-    
-    let toolbar: UIToolbar = {
-        var toolbar: UIToolbar = UIToolbar()
-        toolbar.translatesAutoresizingMaskIntoConstraints = false
-        toolbar.items?.append(UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(handlePlay)))
-        
-        return toolbar
+        return container
     }()
     
-//    override init(style: UITableViewStyle) {
-//        super.init(style: style)
-//
-//        navigationController?.isToolbarHidden = false
-//    }
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+    var fetchedResultsController: NSFetchedResultsController<Survey>!
     
-//    // CoreData: PersistentContainer
-//    lazy var persistentContainer: NSPersistentContainer = {
-//        let container: NSPersistentContainer = NSPersistentContainer(name: "survey")
-//        container.loadPersistentStores(completionHandler: { (description, error) in
-//            if let error = error as NSError? {
-//                fatalError("### persistentContainer UNABLE TO LOAD DATA: \(error)")
-//            }
-//        })
-//
-//        return container
-//    }()
-//
-//    var fetchedResultsController: NSFetchedResultsController<Survey>!
+    // NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK
+    //   NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK NETWORK
+    let serverURL: URL? = URL(string: "survey-app-survey.cloudapps.maltron.solutionarchitectsredhat.com.br")
+    var dataTask: URLSessionDataTask!
     
+    let refresh: UIRefreshControl = {
+        let refreshControl: UIRefreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+
+        return refreshControl
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-//        mytoolbar = UIToolbar()
-//        mytoolbar.translatesAutoresizingMaskIntoConstraints = false
-//        var items = [UIBarButtonItem]()
-//        items.append(UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(handlePlay)))
-//        mytoolbar.setItems(items, animated: true)
-//        view.addSubview(mytoolbar)
-//
-//        NSLayoutConstraint(item: mytoolbar, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
-//        NSLayoutConstraint(item: mytoolbar, attribute: .leading, relatedBy: .equal, toItem: self.view, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
-//        NSLayoutConstraint(item: mytoolbar, attribute: .trailing, relatedBy: .equal, toItem: self.view, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
-        
-        navigationController?.isToolbarHidden = false
+
+        // Setup some Toolbar's buttons
         let items: [UIBarButtonItem] = [
             UIBarButtonItem(barButtonSystemItem: .play, target: self, action: #selector(handlePlay))
         ]
         setToolbarItems(items, animated: true)
         
-        navigationItem.title = "TheCell"
+        // Refresh: Fetch new data and insert into CoreData
+        self.refreshControl = refresh
+        
+        navigationItem.title = "Surveys"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "TheCell")
         tableView.tableFooterView = UIView()
-
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
-        self.refreshControl = refreshControl
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isToolbarHidden = false
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isToolbarHidden = true
     }
     
     @objc func handlePlay() {
@@ -97,6 +116,27 @@ class SurveyViewController: UITableViewController {
     
     @objc func handleRefresh() {
         print("handleRefresh")
+        
+        var request: URLRequest = URLRequest(url: URL(fileURLWithPath: "/api/v1/survey", relativeTo: serverURL))
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        dataTask?.cancel()
+        dataTask = URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    // Network failure
+                    self.refresh.endRefreshing()
+                    print("### NETWORK FAILURE:", error)
+                }
+            } else if let httpResponse = response as? HTTPURLResponse {
+                if httpResponse.statusCode == 200 { // 200 - Ok
+                    
+                } else if httpResponse.statusCode == 204 { // 204 - No Content
+                }
+            }
+        })
+        dataTask?.resume()
     }
     
     override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
